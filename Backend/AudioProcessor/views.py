a2f3c760465783ff4b70d948ce6d5f3c832ecd5e
Rishabh Jain <rishabh.jain9196@gmail.com>
2019-03-09 12:35:26 +0530

    Adding Text to speech - Part1

--- Backend/AudioProcessor/views.py
+++ Backend/AudioProcessor/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hey! This is the Heatmap Back Health Check Endpoint! All looks good here!")
+
+def audio_to_transcript():
+    return
